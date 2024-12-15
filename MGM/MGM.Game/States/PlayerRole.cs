013c34bafb8793ea878402d21bc9de0b499cd84d
damian-bots <tosu0791@gmail.com>
2024-12-15 05:43:21 +0530

    Update PlayerRole.cs

--- MGM/MGM.Game/States/PlayerRole.cs
+++ MGM/MGM.Game/States/PlayerRole.cs
@@ -3,7 +3,7 @@ namespace MGM.Game.States
     public enum PlayerRole
     {
         Citizen,
-        //Police is not used, police use it
+        Police,   
         Doctor,
         Mafia,
     }
